474adffda814ed550abb0e6873003edc1716f823
lusinesargsyanh1 <lusinesargsyanh1@gmail.com>
2020-11-30 08:32:49 +0400

    home page comments, mobile menu

---
+++ assets/js/mobile-menu.js
@@ -0,0 +1,41 @@
+function onClickShowMobileMenu() {
+    var main_menu = document.getElementsByClassName('mobile--menu__left');
+
+    for (var i=0; i<main_menu.length; i++) {
+        main_menu.item(i).classList.add('mobile--menu__left__show');
+        main_menu.item(i).classList.remove('hide--menu');
+    }
+
+    var menu = document.getElementsByClassName('mobile--menu__left__show__main');
+
+    for (var i=0; i<menu.length; i++) {
+        menu.item(i).classList.add('show--menu');
+    }
+
+    var overlay = document.getElementsByClassName('mobile--menu__left__show__overlay');
+
+    for (var i=0; i<overlay.length; i++) {
+       overlay.item(i).classList.add('show--menu');
+    }
+}
+
+function onClickHideMobileMenu(){
+    var main_menu = document.getElementsByClassName('mobile--menu__left');
+
+    for (var i=0; i<main_menu.length; i++) {
+        main_menu.item(i).classList.remove('mobile--menu__left__show');
+    }
+
+    
+    var menu = document.getElementsByClassName('mobile--menu__left__show__main');
+
+    for (var i=0; i<menu.length; i++) {
+        menu.item(i).classList.remove('show--menu');
+    }
+
+    var overlay = document.getElementsByClassName('mobile--menu__left__show__overlay');
+
+    for (var i=0; i<overlay.length; i++) {
+        overlay.item(i).classList.remove('show--menu');
+    }
+}
